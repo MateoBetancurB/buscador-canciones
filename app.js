3ce1eaf949881e9bedcf164530ecc4b634f0c678
Mateo <mateobetancurb@gmail.com>
2022-05-01 10:50:09 -0500

    validaciones en el formulario

---
+++ app.js
@@ -0,0 +1,23 @@
+import * as UI from "./interfaz.js";
+
+UI.formularioBuscar.addEventListener("submit", buscarCancion);
+
+function buscarCancion(e) {
+	e.preventDefault();
+
+	//obtener datos del formulario
+	const artista = document.querySelector("#artista").value;
+	const cancion = document.querySelector("#cancion").value;
+
+	//validaciones de campos
+	if (artista === "" || cancion === "") {
+		UI.divMensajes.textContent = "Todos los campos son obligatorios";
+		UI.divMensajes.classList.add("error");
+
+		setTimeout(() => {
+			UI.divMensajes.textContent = "";
+			UI.divMensajes.classList.remove("error");
+		}, 3000);
+		return;
+	}
+}
